eaa1d9ccc28086dd8dc4d3539f82f44200247381
MufizVora <156405897+MufizVora@users.noreply.github.com>
2024-11-04 11:11:12 +0530

    initial-changes

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Note } from 'src/app/interfaces/note';
+import { NoteService } from 'src/app/services/note.service';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.scss']
 })
-export class NoteListComponent {
+export class NoteListComponent implements OnInit{
+  notes: Note[] = [];
+  @Output() selectedNote = new EventEmitter<Note>();
 
+  constructor(private noteService : NoteService){}
+
+  ngOnInit(): void {
+    this.noteService.getNotesObservable().subscribe((notes: Note[]) => {
+      this.notes = notes;
+    })
+  }
+
+  editNotes(note: Note): void{
+    this.selectedNote.emit(note);
+    this.noteService.setEditable(true);
+  }
+
+  deleteNotes(id: number): void{
+    this.noteService.deleteNotes(id);
+  }
 }
--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { observable, BehaviorSubject, Observable } from 'rxjs';
+import { Note } from '../interfaces/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  private notes: Note[] = [];
+  private noteSubject = new BehaviorSubject<Note[]>([])
+  private isEdit = new BehaviorSubject<boolean>(false);
 
   constructor() { }
+
+  getEditable(){
+    return this.isEdit.asObservable();
+  }
+
+  setEditable(value: boolean){
+    this.isEdit.next(value);
+  }
+
+  getNotesObservable(): Observable<Note[]> {
+    return this.noteSubject.asObservable();
+  }
+
+  createNote(note: Note): void{
+    note.id = new Date().getTime();
+    this.notes.push(note);
+    this.noteSubject.next(this.notes);
+  }
+
+  updateNote(updatedNote: Note): void {
+    const index = this.notes.findIndex((note) => note.id === updatedNote.id);
+    if(index !== -1){
+      this.notes[index] = updatedNote;
+      this.noteSubject.next(this.notes);
+    }
+  }
+
+  deleteNotes(id: number): void{
+    this.notes = this.notes.filter(note => note.id !== id);
+    this.noteSubject.next(this.notes);
+  }
 }
